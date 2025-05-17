a2c8fa09cbc3831cecfb1d4481cc89d0fbca9991
yahn.fs <yahnfreitassantos@gmail.com>
2025-05-17 11:03:30 -0300

    1° etapa
    buildei, apenas.

---
+++ src/main/java/com/o_grupo/agile_account/AgileAccountApplication.java
@@ -0,0 +1,13 @@
+package com.o_grupo.agile_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/o_grupo/agile_account/AgileAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o_grupo.agile_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
